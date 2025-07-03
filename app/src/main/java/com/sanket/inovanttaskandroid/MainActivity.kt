de3c2b93672a668ff678077041fc35f59c4a060e
w1-84209-sanket <sanketwagh3929@gmail.com>
2025-07-03 13:52:53 +0530

    Main

--- app/src/main/java/com/sanket/inovanttaskandroid/MainActivity.kt
+++ app/src/main/java/com/sanket/inovanttaskandroid/MainActivity.kt
@@ -1,16 +1,39 @@
 package com.sanket.inovanttaskandroid
 
+
+
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private var quantity = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
 
+        val minusButton = findViewById<Button>(R.id.minusButton)
+        val plusButton = findViewById<Button>(R.id.plusButton)
+        val quantityText = findViewById<TextView>(R.id.quantityValue)
+
+
+        quantityText.text = quantity.toString()
+
+
+        minusButton.setOnClickListener {
+            if (quantity > 1) {
+                quantity--
+                quantityText.text = quantity.toString()
+            }
+        }
+
+
+        plusButton.setOnClickListener {
+            quantity++
+            quantityText.text = quantity.toString()
+        }
     }
 }
